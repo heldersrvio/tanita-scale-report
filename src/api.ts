48bd2b7891f668e93fa8ca5e46264728da3ae1ec
Helder Sérvio <heldersrvio@gmail.com>
2024-01-16 23:52:44 -0500

    :bug: Fix api url environment variable name

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 export const login = async (email, password) => {
 	const response = await fetch(`${API_URL}/login`, {
